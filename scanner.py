01b4aaf9ede6d4fb37d9fbfa6a68d17fe4f76c48
Andrew Baum <abaum65@pop-os.localdomain>
2022-08-25 11:27:38 -0600

    fix

--- scanner.py
+++ scanner.py
@@ -34,7 +34,7 @@ def main():
         print('-s is required')
         sys.exit()
     else:
-        subnet = args.subnet.subnet.replace('/', '')
+        subnet = args.subnet.replace('/', '')
     if not args.timing:
         timing = str(2)
     else:
